e6c688b1a3f3911445d32a5718f7b30b326541a8
Josue Castro <josuecaztro@gmail.com>
2024-05-11 18:55:13 -0400

    readyforUse

--- scripts.js
+++ scripts.js
@@ -22,34 +22,63 @@ function fixNumberDisplay (number){
     };
 }
 
+
 //THIS IS THE MOST IMPORTANT FUNCTION WITH THE MOST LOGIC
 function startCounter (){
    interval = setInterval(function(){
+
+
+      //Conditionals for TIMER COLOR ALERTS 
+      if (hour === 1 && minute >= 30 && minute < 45){
+        timerContainer.style.background = "rgb(211, 207, 112)";
+       } else if (hour === 1 && minute >= 45) {
+        timerContainer.style.background = "rgb(208, 60, 60)";
+       } else if (hour === 1 && minute >= 50){
+        //flashing red animation????!??!?
+       } else if (hour === 2){
+        clearInterval(interval);
+        timerDisplay.classList.toggle("white-text");
+        timerContainer.classList.toggle("flashing");
+        } else {
+        timerContainer.style.background = "rgb(0, 102, 0)";
+       };
+
     if (second < 60){
         /*it took me forever to figure out the bug on why
         it was skipping the number 9. long story short: i had 
         to turn the (i++) into a (++i) because (i) was getting used
         in the expression before it could be displayed. */
-    timerDisplay.textContent = fixNumberDisplay(hour) + ":" + fixNumberDisplay(minute) + ":" + fixNumberDisplay(++second);
+        ++second;
+    timerDisplay.textContent = fixNumberDisplay(hour) + ":" + fixNumberDisplay(minute);
     }
     //this adds a minute every 60 seconds
     if (second === 60){
         ++minute;
-        timerDisplay.textContent = fixNumberDisplay(hour) + ":" + fixNumberDisplay(minute) + ":" + fixNumberDisplay(0);
+        timerDisplay.textContent = fixNumberDisplay(hour) + ":" + fixNumberDisplay(minute);
         second = 0;
     }
     //this adds an hour every 60 minutes
     if (minute === 60){
         ++hour;
-        timerDisplay.textContent = fixNumberDisplay(hour) + ":" + fixNumberDisplay(0) + ":" + fixNumberDisplay(0);
+        timerDisplay.textContent = fixNumberDisplay(hour) + ":" + fixNumberDisplay(0);
         minute = 0;
     }
    }, 1000);
+   
 
-   timerContainer.style.background = "rgb(0, 102, 0)";
-   timerDisplay.style.color = "white";
+ 
 
 }
+
+
+
+/* ALMOST PERFECT JUST FIX IT SO IT DOESNT RETURN TO GREEN.
+MAYBE JUST STOP AT 2 HOURS AND KEEP FLASHING.
+Green = 1 hour and 30 minutes
+Yellow = in between
+Red = 1 hour and 45 minutes
+*/
+
 
 
 
